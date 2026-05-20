c412a12e690a7e29e6a6ddfb626c833eca8094d8
LoiChamChi <thaovinhloi2007@gmail.com>
2026-05-20 21:32:58 +0700

    updet index.java

--- index.java
+++ index.java
@@ -3,6 +3,10 @@ package Demo;
 public class index {
     public static void main(String[] args) {
         System.out.println("hello world");
-    }
+        int a = 10;
+        int b = 10;
+        System.out.println(a + b);
+
+        }
     
 }
